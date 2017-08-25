2c6809721152e7ffb712691237f5ed75d19da249
Tabitha Hsia <tabithahsia@Tabithas-MBP.attlocal.net>
2017-08-25 10:02:02 -0700

    Initial scaffolding

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      }
+
+    }
+    render() {
+      return(
+        <div>
+          <h1>Hello world!</h1>
+        </div>
+
+      )
+    }
+}
+
+export default App;
