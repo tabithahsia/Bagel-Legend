a0527bfc84a301aa7dcb1753a5f9dfe4dd83783d
Tabitha Hsia <tabithahsia@Tabithas-MacBook-Pro.local>
2017-09-01 16:56:13 -0700

    (feat) Add Signup and Login button

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -8,18 +8,24 @@ import Signup from './Signup.jsx';
 class App extends React.Component {
   constructor(props) {
       super(props);
+      this.state = {
+        showLogin: false
+      }
     }
   render() {
     return(
       <div>
         <h1>Bagel Legend</h1>
         <Search />
-        <Login />
-        <Signup />
+        <button type="button">Login</button>
 
+        { this.state.showLogin ? <Login /> : null }
+
+        <button type="button">Sign Up</button>
+
+        { this.state.showLogin ? <Signup /> : null }
         <div>
           <h2>Top Bagels of All Time</h2>
-
         </div>
 
         <div>
